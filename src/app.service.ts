bf8d919dfde664d2359e61a77c0d2afe776b2892
NAZIR.ALKAHWAJI <team@ventech.co>
2025-01-29 10:40:13 +0300

    .

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,21 +4,6 @@ import axios from 'axios';
 @Injectable()
 export class AppService {
   async getHello() {
-    const senderId = 'City Mall';
-    const numbers = "+962776850132"
-    const accName = 'CityMall';
-    const accPass = 'G_PAXDujRvrw_KoD';
-
-    const smsUrl = `https://josmsservice.com/SMSServices/Clients/Prof/RestSingleSMS_General/SendSMS`;
-    const response = await axios.get(smsUrl, {
-      params: {
-        senderid: senderId,
-        numbers: numbers,
-        accname: accName,
-        AccPass: accPass,
-        msg: encodeURIComponent(`زبوننا العزيز، \n تم تسجيل حالة فقدان الطفل. فرقنا تقوم بعملية البحث. سنبلغكم بأي جديد.\nنتمنى لكم السلامة`),
-      },
-    });
-
+    return "Hello World!"
   }
 }
