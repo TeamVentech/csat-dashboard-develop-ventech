21f28676fa82d5c9dcd2235d776e273640691c3d
a2exf <a2exfr@gmail.com>
2025-05-07 21:04:06 +0200

    fix customer login with otp

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,24 +1,27 @@
-import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { UsersModule } from '../users/users.module';
-import { PassportModule } from '@nestjs/passport';
-import { JwtModule } from '@nestjs/jwt';
-import { JwtStrategy } from './jwt.strategy';
-import { AuthController } from './auth.controller';
-import { LocalStrategy } from './local.strategy';
-import { RolesModule } from '../roles/roles.module';
+import { Module } from '@nestjs/common'
+import { AuthService } from './auth.service'
+import { UsersModule } from '../users/users.module'
+import { CustomersModule } from 'customers/customers.module'
+import { PassportModule } from '@nestjs/passport'
+import { JwtModule } from '@nestjs/jwt'
+import { JwtStrategy } from './jwt.strategy'
+import { AuthController } from './auth.controller'
+import { LocalStrategy } from './local.strategy'
+import { RolesModule } from '../roles/roles.module'
 
 @Module({
-  imports: [
-    UsersModule,
-    PassportModule,
-    RolesModule,
-    JwtModule.register({
-      secret: process.env.JWT_SECRET || 'defaultSecretKey', // Use environment variable for production
-      signOptions: { expiresIn: '3h' }, // Token expiration
-    }),
-  ],
-  providers: [AuthService, JwtStrategy, LocalStrategy],
-  controllers: [AuthController],
+	imports: [
+		UsersModule,
+		PassportModule,
+		RolesModule,
+		JwtModule.register({
+			secret: process.env.JWT_SECRET || 'defaultSecretKey', // Use environment variable for production
+			signOptions: { expiresIn: '3h' }, // Token expiration
+		}),
+		CustomersModule,
+	],
+	providers: [AuthService, JwtStrategy, LocalStrategy],
+	controllers: [AuthController],
 })
-export class AuthModule {}
+export class AuthModule {
+}
--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,20 +1,37 @@
-import { Injectable } from '@nestjs/common';
-import { ConfigService } from '@nestjs/config';
-import { PassportStrategy } from '@nestjs/passport';
-import { ExtractJwt, Strategy } from 'passport-jwt';
-import { UsersService } from '../users/users.service';
+import { Injectable, UnauthorizedException } from '@nestjs/common'
+import { ConfigService } from '@nestjs/config'
+import { PassportStrategy } from '@nestjs/passport'
+import { ExtractJwt, Strategy } from 'passport-jwt'
+import { UsersService } from '../users/users.service'
+import { CustomersService } from '../customers/customers.service'
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor(private readonly usersService: UsersService, configService: ConfigService) {
-    super({
-      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: configService.get('JWT_SECRET'),
-    });
-  }
+	constructor(
+		private readonly usersService: UsersService,
+		configService: ConfigService,
+		private customersService: CustomersService,
+	) {
+		super({
+			jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+			secretOrKey: configService.get('JWT_SECRET'),
+		})
+	}
 
-  async validate(payload: { id: string }) {
-    
-    return this.usersService.findOne(payload.id);
-  }
+	async validate(payload: any) {
+		if (payload.role === 'customer') {
+			const customer = await this.customersService.findOne(payload.id)
+			console.log(customer)
+			if (!customer) {
+				throw new UnauthorizedException('Customer not found')
+			}
+			return Object.assign(customer, { role: 'Regular User' })
+		} else {
+			const user = await this.usersService.findOne(payload.id)
+			if (!user) {
+				throw new UnauthorizedException('User not found')
+			}
+			return user
+		}
+	}
 }
