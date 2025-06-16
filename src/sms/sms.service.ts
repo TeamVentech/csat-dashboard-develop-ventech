f633a64a96fa847227af5c09bb40236fb23aeea3
a2exf <a2exfr@gmail.com>
2025-06-16 13:10:10 +0200

    .

--- src/sms/sms.service.ts
+++ src/sms/sms.service.ts
@@ -1,29 +1,31 @@
-import { Injectable } from '@nestjs/common';
-import axios from 'axios';
+import { Injectable } from '@nestjs/common'
+import axios from 'axios'
 
 @Injectable()
 export class SmsService {
-  async sendSms(data: any, message: string, number: string) {
-    const senderId = 'City Mall';
-    const numbers = number;
-    const accName = 'CityMall';
-    const accPass = 'G_PAXDujRvrw_KoD';
+	async sendSms(data: any, message: string, number: string) {
+		const senderId = 'City Mall'
+		const numbers = number
+		const accName = 'CityMall'
+		const accPass = 'G_PAXDujRvrw_KoD'
 
-    const smsUrl = `https://josmsservice.com/SMSServices/Clients/Prof/RestSingleSMS_General/SendSMS`;
-    try {
-      const response = await axios.get(smsUrl, {
-        params: {
-          senderid: senderId,
-          numbers: numbers,
-          accname: accName,
-          AccPass: accPass,
-          msg: encodeURIComponent(message),
-        },
-      });
-      return response.data;
-    } catch (error) {
-      console.error(`Failed to send SMS to ${number}:`, error);
-      throw error;
-    }
-  }
-} 
+		// const smsUrl = `https://josmsservice.com/SMSServices/Clients/Prof/RestSingleSMS_General/SendSMS`;
+		const smsUrl = 'https://www.josms.net/SMSServices/Clients/Prof/SingleSMS_General/SMSService.asmx/SendSMS'
+		try {
+			const response = await axios.get(smsUrl, {
+				params: {
+					senderid: senderId,
+					numbers: numbers,
+					accname: accName,
+					AccPass: accPass,
+					msg: encodeURIComponent(message),
+					id: '',
+				},
+			})
+			return response.data
+		} catch (error) {
+			console.error(`Failed to send SMS to ${number}:`, error)
+			throw error
+		}
+	}
+}
